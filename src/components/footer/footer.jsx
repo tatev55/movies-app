b637911c3b9604c0d730ac22595493c09664b4e9
tatev55 <tatevmargaryan555@gmail.com>
2025-02-27 21:58:16 +0400

    add css styles

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -7,7 +7,8 @@ const Footer =() => {
         <div className="footer-container">
             <div className="footer-content">
                 <div className="footer-text">
-                    <p>"Netflix is a streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices.You can watch as much as you want, whenever you want without a single commercial – all for one low monthly price. There's always something new to discover and new TV shows and movies are added every week!"</p>
+                    <p className = "about-text">© 2025 MovieHub. All Rights Reserved.
+                    Disclaimer: The content on this site is for entertainment purposes only. All movie titles, posters, and images are the property of their respective owners.</p>
                     <p className="phoneNumber"><strong>Phone: </strong>+3377990011</p>
                     <p className="emailAddress"><strong>Email:</strong>moviespage@gmail.com</p>
                 </div>
